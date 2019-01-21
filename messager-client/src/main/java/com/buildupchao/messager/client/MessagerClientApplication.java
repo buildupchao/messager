c407c6d11a52d4e595c58e8117cf5f655f939678
Zychaowill <15536542139@163.com>
2019-01-22 01:11:34 +0800

    Provide messager client.

---
+++ messager-client/src/main/java/com/buildupchao/messager/client/MessagerClientApplication.java
@@ -0,0 +1,17 @@
+package com.buildupchao.messager.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author buildupchao
+ *         Date: 2019/1/22 01:10
+ * @since JDK 1.8
+ */
+@SpringBootApplication
+public class MessagerClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagerClientApplication.class, args);
+    }
+}
