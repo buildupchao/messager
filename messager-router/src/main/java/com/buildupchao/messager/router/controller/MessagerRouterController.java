32530aa0b2bc813eee241ecba602a284443a76d6
Zychaowill <15536542139@163.com>
2019-01-22 00:46:43 +0800

    Fix bug for register function.

--- messager-router/src/main/java/com/buildupchao/messager/router/controller/MessagerRouterController.java
+++ messager-router/src/main/java/com/buildupchao/messager/router/controller/MessagerRouterController.java
@@ -78,6 +78,7 @@ public class MessagerRouterController {
         Long userId = System.currentTimeMillis();
         RegisterResVO registerResInfo = new RegisterResVO();
         registerResInfo.setUserId(userId);
+        registerReqInfo.setUserName(registerReqInfo.getUserName());
         registerResInfo = accountService.register(registerResInfo);
 
         return ResponseHelper.createSuccess(registerResInfo);
